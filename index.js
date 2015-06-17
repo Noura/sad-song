98e784a0ee2caf321487435e3bf3f776687d09db
Noura <culurienneldoreth@gmail.com>
2015-06-16 19:27:32 -0700

    noise amount updates with changing GSR values

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ window.onload = function() {
     var data = null;
     $.getJSON('data.json', function(d) {
         data = d;
-        console.log('data', data);
     });
 
     var context = new AudioContext();
@@ -14,7 +13,6 @@ window.onload = function() {
     request.onload = function() {
         context.decodeAudioData(request.response, function(buf) {
             buffer = buf;
-            console.log('buffer', buffer);
         }, function error() {
             console.log('error with request for song');   
         });
@@ -29,26 +27,40 @@ window.onload = function() {
         var source = context.createBufferSource();
         source.buffer = buffer;
 
-        var whiteNoise = context.createScriptProcessor(4096, 2, 2);
-        whiteNoise.noise_level = 0.01;
-        whiteNoise.onaudioprocess = function(e) {
+        var filter = context.createScriptProcessor(4096, 2, 2);
+        filter.gsr = 0;
+        filter.onaudioprocess = function(e) {
             for (var channel = 0; channel < e.outputBuffer.numberOfChannels; channel++) {
                 var input = e.inputBuffer.getChannelData(channel);
                 var output = e.outputBuffer.getChannelData(channel);
                 for (var sample = 0; sample < e.inputBuffer.length; sample++) {
-                    output[sample] = input[sample];
-                    output[sample] += ((Math.random() * 2) - 1) * whiteNoise.noise_level;
+                    output[sample] = input[sample] * (1 - filter.gsr) + ((Math.random() * 2) - 1) * filter.gsr;
                 }
             }
         };
-        source.connect(whiteNoise);
-        whiteNoise.connect(context.destination);
+        source.connect(filter);
+        filter.connect(context.destination);
 
         source.start(0);
 
-        setInterval(function() {
-            whiteNoise.noise_level = Math.random() * 0.2;
-        }, 500);
+        // TODO sync the data with the song listening
+        // the song ended at 4:43 from the end on the data recording
+        var i = 0;
+        var gsr = _.pluck(data, 'gsr');
+        var min = _.min(gsr);
+        var max = _.max(gsr);
+        gsr = _.map(gsr, function(g) {
+            return (g - min) / (max - min);
+        });
+        function change_noise_level() {
+            if (i >= data.length) {
+                return;
+            }
+            filter.gsr = gsr[i];
+            i += 1;
+            setTimeout(change_noise_level, 50);
+        };
+        change_noise_level();
     }
     proceed_when_ready();
 };
